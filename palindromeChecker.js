9acb8fe2d48e3aaa2536965c63b6b8e2263a781f
Felipe Meneses Graca <fmeneses-graca@myseneca.ca>
2022-10-16 22:17:27 -0400

    Update palindromeChecker.js

--- palindromeChecker.js
+++ palindromeChecker.js
@@ -12,14 +12,14 @@ function palindromeChecker(str) {
     let newStr = str.toLowerCase().replace(/[^a-z0-9]/gi,"").split("")
   
     //separate array in half, reverse the secondHalf, join to turn to string, and compare. If equal, return true, if different, return false.
-    let half = newStr.length / 2;
-    let firstHalf = newStr.splice(0, half).join("");
-    let secondHalf = newStr.splice(-half).reverse().join("");
+    let half = newStr.length / 2
+    let firstHalf = newStr.splice(0, half).join("")
+    let secondHalf = newStr.splice(-half).reverse().join("")
   
     if (firstHalf === secondHalf) {
-      return true;
+      return true
     }
-    return false;
+    return false
   }
   
-  console.log(palindromeChecker("racecar"));
+  console.log(palindromeChecker("racecar"))
